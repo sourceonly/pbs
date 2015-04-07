86bb5659fea911b8266bb3637ba86badf16b7128
sourceonly <sourceonly@gmail.com>
2015-04-07 15:22:24 +0800

    1. refresh template for platform

---
+++ pbs/refresh/refresh.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python 
+import sys
+sys.path.append(scriptDir)
+import pbs_tools
+import RefreshUtils
+
+
+opti_refresh=pbs_tools.RefreshModule( applicationArgs, refreshSourceName);
+
+opti_refresh.create_platform();

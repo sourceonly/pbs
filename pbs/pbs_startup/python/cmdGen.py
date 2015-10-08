722430284a4420b9adb0a01de19ed2911643c77a
sourceonly <sourceonly@gmail.com>
2015-10-08 17:44:11 +0800

    1. cmdGen's only work is to replace a env (typical a dict ) to create a new command .

---
+++ pbs/pbs_startup/python/cmdGen.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+'''
+
+'''
+
+
+
+import re
+class cmdGen: 
+	def __init__(self):
+		self.data_table={};
+		self.data_table['EXEC']='Optistruct'
+		self.data_table['CPU']='10'
+		self.data_table['MEM']='10'
+		self.template=[];
+		
+	def update_data_table(self,key,value): 
+		self.date_table[key]=value;
+	def update_template(self,template_list): 
+		self.template=template_list;
+	def convert_argv(self,argv): 
+		pass;
+
+
+	def strip_argv(self,argv): 
+		if self.is_argv_optional(argv):
+			return argv[1:];
+		return argv;
+	def is_argv_optional(self,argv): 
+		if argv[0]=='?': 
+			return True;
+		return False
+
+	def symbol_sub (self,argv, dict): 
+		re_symbol=re.compile("@([A-Za-z\-_\+]+)@"); 
+		symbol_match=True
+		while (symbol_match) : 
+			symbol_match=re_symbol.search(argv)
+			if symbol_match: 
+				key=symbol_match.group(1);
+				if not dict.has_key(key): 
+					if self.is_argv_optional(argv): 
+						return '' ;
+					break
+				re_this=re.compile("@"+key+"@")
+				argv=re.sub(re_this,dict[key],argv)
+					
+		return self.strip_argv(argv)
+		
+	def symbol_replace(self,argv):
+		print self.data_table
+		return self.symbol_sub(argv,self.data_table);
+
+	def list_sub(self,list): 
+		for i in range(len(list)): 	
+			list[i]=self.symbol_replace(list[i]);
+		return list
+
+'''
+b=cmdGen();
+a={}
+
+a['EXEC']="AAA"
+a['CPU']="ABC"
+print b.list_sub(['@EXEC@','-cpu @CPU@','?-len @MEM@'])
+'''

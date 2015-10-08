d9ff1c9ca06a0b1cb45c59bd9fcb7bfc79e9e9d1
sourceonly <sourceonly@gmail.com>
2015-10-08 21:00:59 +0800

    1. change taskManage so that it only deal with exec calls

--- pbs/pbs_startup/python/taskManage.py
+++ pbs/pbs_startup/python/taskManage.py
@@ -11,39 +11,6 @@ class taskManage():
         self.rc=1;
         self.history=[];
 
-    def make_cmd (self, cmd_template):
-        cmd=[];
-        for i in cmd_template:
-            segment=self.make_word(i);
-            if segment != '' :
-                cmd.append(self.make_word(i));
-        return cmd
-
-    def make_word (self, string):
-        optional_flag=False;
-        key_not_exist_flag=False;
-        key_empty_flag=False;
-        if string[0]=='?':
-            optional_flag=True;
-        res_string=re.sub("^\?","",string);
-        reg_key=re.compile("@([A-Za-z0-9_\-]+)@");
-        while True:
-            key_match=reg_key.search(res_string);
-            if not key_match:
-                break
-            key=key_match.group(1);
-            replace_key=re.compile("@"+key+"@");
-            if not self.para.has_key(key):
-                key_not_exist_flag=True;
-                res_string=replace_key.sub("",res_string);
-            else:
-                if str(self.para[key])=='' :
-                    key_empty_flag=True;
-                res_string=replace_key.sub(str(self.para[key]),res_string);
-                
-        if optional_flag and key_not_exist_flag:
-            return '';
-        return res_string;
     
     def run_cmd (self, cmd, bg=False):
         self.history.append(" ".join(cmd));
@@ -57,48 +24,10 @@ class taskManage():
         ptask=subprocess.Popen(cmd,shell=False,stdout=subprocess.PIPE)
         return ptask.communicate();
         
-    def run_app(self, cmd_template, para_dict): 
-        self.para=para_dict;
-        cmd=self.make_cmd(cmd_template);
-	try: 
-		return self.run_cmd(cmd);
-
-	except: 
-		print cmd
     
 
 
 
-        
-        
-        
-class Optistruct(taskManage) :
-    def __init__ (self):
-        taskManage.__init__(self);
-        self.command_template="@OPTISTRUCT@ -len @MEM@ -cpu @NCPUS@ @FEM_FILE@ ?@EXTRA_OPTION@"
-        self.license='6200@localhost'
-        self.para['NCPUS']=1
-        self.para['MEM']='100M'
-        self.para['OPTISTRUCT']='/usr/local/bin/optistruct';
-        self.para['EXTRA_OPTION']=' -test abc -j=222 -K"574"'
-
-    def run(self):
-        os.environ['ALTAIR_LICENSE_PATH']=self.license;
-        return self.run_cmd(self.command_template);
-
-class Fluent(taskManage):
-    def __init__ (self):
-        taskManage.__init__(self);
-        self.command_template="@FLUENT@ -t@NCPUS@ -cnf=@HOSTFILE@ -ssh @BACKGROUND@ ?@IB@ @EXTRA_OPTION@ ?@REDIRECTION@"
-        self.license='6200@localhost'
-        self.para['IB']='-pib'
-        self.para['FLUENT']='optistruct'
-        self.para['NCPUS']=10
-        self.para['BACKGROUND']='-g'
-        self.para['REDIRECTION']=' > a.out'
-        self.para['HOSTFILE']='$PBS_NODEFILE'
-    
-        
         
         
         

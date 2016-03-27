c7fd7a6ba557e0e4de1a59740a508e1707b05d4c
sourceonly <sourceonly@gmail.com>
2016-03-28 01:27:57 +0800

    1. add and/or pattern to filter action

--- pbs/tools/pattern.py
+++ pbs/tools/pattern.py
@@ -4,18 +4,26 @@
 
 class pattern () : 
 	def __init__(self): 
+		self.type='or'	
 		return 
+	def set_pattern_type(self,type='or'): 
+		self.type=type
 	def __map__(self,table): 
 		keys=table.keys();
 		keys.sort();
 		return keys
 	def __filter__(self,obj,filter) : 
 		for f_key in filter: 
+			print f_key
 			if not obj.has_key(f_key): 
 				return {}
 			else: 
-				if len(list(set(obj[f_key]+filter[f_key]))) == len(obj[f_key])+len(filter[f_key]): 
-					return {}
+				if self.type=='or':		
+					if len(list(set(obj[f_key]+filter[f_key]))) == len(obj[f_key])+len(filter[f_key]): 
+						return {}
+				elif self.type=='and': 
+					if len(list(set(obj[f_key]+filter[f_key]))) != len(obj[f_key]):
+						return {}
 		return	obj
 	def __acc__(self,init_value,key,obj): 
 		if obj== {} : 
--- pbs/tools/test_sched.py
+++ pbs/tools/test_sched.py
@@ -10,12 +10,27 @@ filter['Job_Name']=['aa','Hyperworks','STDIN']
 filter['queue']=['iworkq','workq']
 res_table={}
 pattern=pattern.pattern();
-for i in pattern.__map__(t): 
-	obj=t[i];
-	res=pattern.__filter__(obj,filter);
-	pattern.__acc__(res_table,i,res);
 
-a=res_table.keys()
+#for i in pattern.__map__(t): 
+#	obj=t[i];
+#	res=pattern.__filter__(obj,filter);
+#	pattern.__acc__(res_table,i,res);
+
+#a=res_table.keys()
+#a.sort()
+#print a
+	
+
+
+node_table={}
+n=pt.table['pbsnodes']
+n_filter={}
+n_filter['resources_available.pas_applications_enabled']=['Optistruct']
+for j in pattern.__map__(n): 
+	obj=n[j]
+	res=pattern.__filter__(obj,n_filter);
+	pattern.__acc__(node_table,j,res);
+
+a=node_table.keys()
 a.sort()
 print a
-	
